e944d5ecb22307f5c99f99b25def8d1b9627a88a
karthe1010 <karthick@iplon.de>
2017-03-30 12:37:38 +0530

    added missing examples

---
+++ 01_helloWorld/main.go
@@ -0,0 +1,23 @@
+/*////////////////////////////////////////////////////////////////////////////////
+//                                                                              //
+// Author  : Karthick K                                                         //
+// Project : GO Example                                                         //
+// Program : Hello World                                                        //
+// Date    : 22-03-2017                                                         //
+//                                                                              //
+////////////////////////////////////////////////////////////////////////////////*/
+
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+
+// define package
+package main
+
+// impot fmt package
+import "fmt"
+
+// function main starts here
+func main() {
+	// print function
+	fmt.Println("Hello, World!\n")
+}
